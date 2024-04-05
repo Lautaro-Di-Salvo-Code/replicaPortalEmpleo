d532da907c93d7897107d5677d654b4e28fc4a84
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-04-05 13:40:00 -0300

    correccion de enlace

--- src/Componentes/HomeHijos/HoleHeadear.jsx
+++ src/Componentes/HomeHijos/HoleHeadear.jsx
@@ -23,7 +23,7 @@ export const HoleHeader = ({header, Navbar}) => {
        <NavLink className={({ isActive}) => isActive ? "LinksNav" : null } to="/Home">{Navbar.op1}</NavLink>
        <NavLink className={({ isActive}) => isActive ? "LinksNav" : null } to="/2"> {Navbar.op2}</NavLink>
 
-       <a href="https://melodic-starlight-6a26e2.netlify.app/Home">Of Empleo y Agencias Territoriales</a>
+       <a href="https://melodic-starlight-6a26e2.netlify.app">Of Empleo y Agencias Territoriales</a>
 
        <NavLink className={({ isActive}) => isActive ? "LinksNav" : null } to=""> {Navbar.op4}</NavLink>
    </div>
